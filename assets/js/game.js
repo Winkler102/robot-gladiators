513fe48eb71ed0422ec871d4ccd1bfb5f5352196
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-03-24 14:54:35 -0500

    created javascript and html files

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is you robot's name?");
+
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+console.log("Tony the Robot" + " is ready for battle!");
+console.log(playerName + " is ready for battle!");
+console.log("Your robot, " + playerName + ", has won!");
+
+// fight();
